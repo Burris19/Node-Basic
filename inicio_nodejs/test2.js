7187fbbc8b59d1c607d321a612d1d28ee2a58ba3
Burris19 <julianhernandez19@gmail.com>
2016-03-11 10:36:17 -0600

    [add] introduction JavaScript

---
+++ inicio_nodejs/test2.js
@@ -0,0 +1,12 @@
+var fact = 0;
+var resp = 0;
+
+fact = 5;
+resp = 1;
+
+for (var i = 1; fact >= i; i++) {
+	resp = resp * i;
+	console.log(" Iteracion " + i + " : " + " resp :" +resp);
+}
+
+console.log("El factorial de numero " + fact + " es : " + resp);
