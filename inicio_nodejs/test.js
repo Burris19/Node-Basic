180b41a75a1138db7d559f0822d87dadba9e3213
Burris19 <julianhernandez19@gmail.com>
2016-03-11 10:35:53 -0600

    [add] introduction JavaScript

---
+++ inicio_nodejs/test.js
@@ -0,0 +1,10 @@
+var ahorro = 0;
+var meses = 0;
+var cantidad = 0;
+
+ahorro=60;
+meses=48;
+cantidad =ahorro * meses;
+
+console.log("El dia de hoy es :" + new Date());
+console.log("La cantidad ahorrada en " + meses + " es " + cantidad)
