59ce1fbb4282afac0294ec1eb4b74660cba8b26c
Burris19 <julianhernandez19@gmail.com>
2016-03-11 10:37:34 -0600

    [add] struct basic server

---
+++ server_web_node/serve.js
@@ -0,0 +1,12 @@
+var http=require("http");
+
+http.createServer(
+
+	function(request, response){
+		response.writeHead(200,{'Content-Type':'text/plain'});
+		response.end('Bienvenido al curso de node.js');
+	}	
+
+).listen(8000);
+
+console.log("Server inicializado...")
