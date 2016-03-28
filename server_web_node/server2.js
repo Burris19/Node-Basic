6f6f532751834f274fed4810e684ca6527cca0eb
Burris19 <julianhernandez19@gmail.com>
2016-03-28 13:23:26 -0600

    [fix] Error correction

--- server_web_node/server2.js
+++ server_web_node/server2.js
@@ -1,20 +1,12 @@
 var http=require("http");
-var url=require("url");
 
 http.createServer(
 
 	function(request, response){
 		response.writeHead(200,{'Content-Type':'text/plain'});
-
-		var params = url.parse(request.url, true).query;
-		var input = params.number;
-		var numImput = new Number(input);
-		var numOutput = new Number(Math.random()*numImput).toFixed(0);
-
-		response.write(numOutput);
-		response.end();
+		response.end('Bienvenido al curso de node.js');
 	}	
 
 ).listen(8000);
 
-console.log("Server inicializado...")
+console.log("Server inicializado...");
